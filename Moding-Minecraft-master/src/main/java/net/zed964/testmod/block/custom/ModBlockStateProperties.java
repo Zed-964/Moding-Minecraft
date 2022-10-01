82e6079a8bb5b6b2edebef89a53e5fbad5c90916
Victo0ri <113626801+Victo0ri@users.noreply.github.com>
2022-10-01 17:42:22 +0200

    Delete Moding-Minecraft-master directory

--- Moding-Minecraft-master/src/main/java/net/zed964/testmod/block/custom/ModBlockStateProperties.java
+++
@@ -1,8 +0,0 @@
-package net.zed964.testmod.block.custom;
-
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-
-public class ModBlockStateProperties extends BlockStateProperties {
-    public static final EnumProperty<VerticalSlabType> VERTICAL_SLAB_TYPE = EnumProperty.create("type", VerticalSlabType.class);
-}
